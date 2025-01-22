76f43cc393d355326bbf33eebe88e4294ba87be7
jchung02 <joaniechung2821@gmail.com>
2025-01-22 16:59:59 +0000

    commit하는 연습 ...

--- MNIST_Flax.py
+++ MNIST_Flax.py
@@ -6,3 +6,23 @@ tf.random.set_seed(0)
 train_steps = 1200
 eval_every = 200
 batch_size = 32
+
+train_ds: tf.data.Dataset = tfds.load('mnist', split='train')
+test_ds: tf.data.Dataset = tfds.load('mnist', split='test')
+
+train_ds = train_ds.map(
+  lambda sample: {
+    'image': tf.cast(sample['image'], tf.float32) / 255,
+    'label': sample['label'],
+  }
+)  # normalize train set
+test_ds = test_ds.map(
+  lambda sample: {
+    'image': tf.cast(sample['image'], tf.float32) / 255,
+    'label': sample['label'],
+  }
+)
+
+train_ds = train_ds.repeat().shuffle(1024)
+train_ds = train_ds.batch(batch_size, drop_remainder=True).take(train_steps).prefetch(1)
+test_ds = test_ds.batch(batch_size, drop_remainder=True).prefetch(1)
